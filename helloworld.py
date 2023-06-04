b66c7fb531f794e8da0d086ba5253f85eddb6934
root <root@ip-172-31-18-99.ec2.internal>
2023-06-04 12:12:32 +0000

    debuged the code

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,49 @@
 print('Hello,World!')
 
+class Python:
+
+   def __init__(self):
+
+         print('welcome!')
+
+   def func(x,y):
+
+       if x==5 or y==10:
+
+            return x,y
+
+       else:
+
+           return x
+
+
+
 X=5
+
 if X >2:
+
    print('5 is greter than 2')
+
 else:
+
    print('X is less than 2')
 
+
+
 #python
+
+
+
+#class
+
+#programming
+
+obj=Python()
+
+print(obj,type(obj))
+
+z=Python.func(5,9)
+
+print(z)
+
+#call
